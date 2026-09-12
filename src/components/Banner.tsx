a7aa74ebbbb26047bc935fcc60bbe2cd14b9d7db
rezasdsju <rezaul2002104@gmail.com>
2026-09-12 12:45:02 +0600

    style: update Banner UI

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -12,7 +12,7 @@ const Banner = () => {
         <div className='grid grid-cols-1 sm:grid-cols-12 items-center px-3 sm:px-7 bg-linear-to-br from-blue-200 via-purple-50 to-cyan-100 py-4 sm:py-15 ' >
             <div className='sm:col-span-8 flex flex-col items-center text-center'>
                 <h2 className='text-black text-2xl sm:text-4xl '>School of Analytics and Technology</h2>
-                <p className='text-black text-center '>Bridge to Efficient Thinking & Advancement</p>
+                <p className='bg-linear-to-r from-red-600 to-red-900 bg-clip-text text-transparent text-center '>Bridge to Efficient Thinking & Advancement</p>
                 <div className='w-full  text-justify mt-6 text-blue-900'>
                     <p>The School of Analytics and Technology (SAT) is a premier learning platform dedicated to empowering learners with in-demand skills in programming, data analysis, research, and much more. SAT is designed to provide a seamless and engaging learning experience through a variety of practical tools and resources.</p>
                 </div>
